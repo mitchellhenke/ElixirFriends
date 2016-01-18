2a32624e73894dfddabf84460ba6fbe0bda32425
Mitchell Henke <mitchh23@gmail.com>
2016-01-17 18:51:21 -0600

    try not importing prod config secret

--- config/prod.exs
+++ config/prod.exs
@@ -66,4 +66,4 @@ config :elixir_friends, ElixirFriends.Repo,
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
