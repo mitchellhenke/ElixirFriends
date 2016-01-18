20934e62e77ece488494ab0e406cba2995f44291
Mitchell Henke <mitchh23@gmail.com>
2016-01-17 18:40:39 -0600

    get postgres info from env

--- config/test.exs
+++ config/test.exs
@@ -12,6 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :elixir_friends, ElixirFriends.Repo,
   adapter: Ecto.Adapters.Postgres,
+  username: System.get_env("DATABASE_POSTGRESQL_USERNAME") || "mitchellhenke",
+  password: System.get_env("DATABASE_POSTGRESQL_PASSWORD") || "",
   database: "elixir_friends_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
